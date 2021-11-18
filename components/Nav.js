50f0799b09f45375b1a2408af254606df4fcb42b
zbarnz <zbarnz@yahoo.com>
2021-11-18 00:11:23 -0700

    vercel fix 2 (linting errors)

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Link from "next/link"
+import Link from "next/link";
 import styles from "../styles/Nav.module.css";
 
 const Menu = () => {
   return (
     <div className={styles.navContainer}>
-      <h3><Link href="/"><b>SOCNET</b></ Link> Zach's Portfolio</h3>
+      <h3><Link href="/"><b>SOCNET</b></ Link> Zach&apos;s Portfolio</h3>
     </div>
   );
 };
